a897738e714027f751dc27336ab28ed922948bd6
Abhishekkp42 <96106223+Abhishekkp42@users.noreply.github.com>
2022-05-29 14:44:29 +0530

    circles and empty div changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,35 @@ import logo from './logo.svg'
 import styled from "styled-components";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let arr=[
+    {
+      id: 1,
+      color: "#d5e8d4",
+      status: false
+    },
+    {
+      id: 2,
+      color: "#ffe6cd",
+      status: false
+    },
+    {
+      id: 3,
+      color: "#6a00ff",
+      status: false
+    },
+    {
+      id: 4,
+      color: "#d9e8fb",
+      status: false
+    },
+    {
+      id: 5,
+      color: "#e2d5e7",
+      status: false
+    }
+  ]
+
 
   const Container = styled.div`
   display: flex;
@@ -24,16 +52,27 @@ function App() {
     <Container>
       <div style={{marginTop: "120px", marginRight: "200px"}}>
         <p>Empty div</p>
-        <div style={{border: "2px solid black", height: "350px", width: "300px"}}></div>
+        <div style={{border: "2px solid black", height: "350px", width: "300px"}}>
+        {arr.map((el) => {
+          return el.status ? 
+          <Circle key={el.id} style={{backgroundColor:`${el.color}` }}></Circle>
+          : null
+        })}
+        </div>
       </div>
 
       <div>
-        <p>Circles</p>
+        {/* <p>Circles</p>
         <Circle style={{backgroundColor:"#d5e8d4" }} ></Circle>
         <Circle style={{backgroundColor:"#ffe6cd" }} ></Circle>
         <Circle style={{backgroundColor:"#6a00ff" }} ></Circle>
         <Circle style={{backgroundColor:"#d9e8fb" }} ></Circle>
-        <Circle style={{backgroundColor:"#e2d5e7" }} ></Circle>
+        <Circle style={{backgroundColor:"#e2d5e7" }} ></Circle> */}
+
+        {arr.map((el) => {
+          return el.status ? null :
+          <Circle key={el.id} style={{backgroundColor:`${el.color}` }}></Circle>
+        })}
       </div>
       
       <div>
